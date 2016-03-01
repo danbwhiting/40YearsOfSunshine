739385b94d98a90159f8160a123cf87c0d3a7d12
danbwhiting <dan@danwhiting.com>
2016-02-29 22:37:04 -0500

    Mailto

--- js/custom.js
+++ js/custom.js
@@ -1599,5 +1599,5 @@ function showRemaining() {
 timer = setInterval(showRemaining, 1000);
 
 $('document').ready(function() {
-	$('#dan').html('dan@danwhiting.com.');
+	$('#dan').html('<a href=\"mailto:dan@danwhiting.com?subject=Sunshine\">dan@danwhiting.com</a>');
 });
